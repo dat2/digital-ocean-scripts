ba2f1f0bdd3c4b54919be4663237eb139fd3907b
Nicholas Dujay <nickdujay@gmail.com>
2016-09-15 00:59:31 +0000

    comment setup_git_deploy script to explain what it does

--- setup_git_deploy.sh
+++ setup_git_deploy.sh
@@ -5,10 +5,13 @@
 
 RET_DIR=`pwd`
 
+# clone the bare repo to /var/build/$2
 git clone --bare $1 /var/repo/$2.git
 cd /var/repo/$2.git
 mkdir -p /var/build/$2
 
+# setup a hook to automatically clone repo contents into /var/build/$2
+# then run the deploy_haskell_folder script in the /var/build/$2 folder
 cd hooks
 cat "#!/bin/bash\ngit-work-tree=/var/build/$2 --git-dir=/var/repo/$2.git checkout -f\ndeploy_haskell_folder.sh /var/build/$2 /$2" > post-receive
 chmod +x post-receive
